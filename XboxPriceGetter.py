18f98cbf24750605a5d17f13fda0fb76057fc08c
Nimtaa <nima.tavassoli2@gmail.com>
2018-09-11 01:44:52 +0430

    zip iteration added

--- XboxPriceGetter.py
+++ XboxPriceGetter.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import  requests
+
+
 link = "https://www.digikala.com/search/?q=xbox&sortby=4";
 r = requests.get(link)
 data = r.content;
@@ -7,8 +9,8 @@ data = r.content;
 soup = BeautifulSoup(data);
 
 xboxs_name = soup.select(".c-product-box__title a");
-xboxs_price = soup.select(".c-product-box__content");
-for x in xboxs_price:
-    if("Xbox" in x.text):
-        print(x.text);
+xboxs_price = soup.select(".c-price__value");
+for price, name in zip(xboxs_price,xboxs_name):
+    if("Xbox" in name.text):
+        print(name.text + "  : "  + price.text);
 
