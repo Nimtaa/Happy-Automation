7108fed28ec0282c9ec0536b1c915ddc7da63646
Nimtaa <nima.tavassoli2@gmail.com>
2018-09-15 22:13:50 +0430

    get the Marzdaran area traffic image

--- traffic.py
+++ traffic.py
@@ -2,19 +2,11 @@ from bs4 import BeautifulSoup
 import  requests
 import re
 from PIL import Image
+import urllib.request
+import datetime
 
 
-
-
-link = "http://31.24.237.150/TTCCTrafficWebSite/PublicUsers/GraphicalTrafficMap/Default.aspx";
-r = requests.get(link)
-data = r.content;
-
-soup = BeautifulSoup(data);
-file = open('marzdaran_square.jpg', 'w')
-marzdaran_square = soup.select("#tdImage102");
-image = Image.open(marzdaran_square)
-image.show()
-
-file.write(marzdaran_square);        # print((re.findall(r'\d*',price)));
-file.close()
+#marzdaran Highway
+urllib.request.urlretrieve("http://31.24.237.150/TTCCTrafficWebSite/UploadedFiles/WebTrafficImages/Web102.png",
+ str(datetime.datetime.now())+".png");
+print("sal")
